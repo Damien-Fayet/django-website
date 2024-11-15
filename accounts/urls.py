c48ad64e4f2b9ddb06a7aa9faa6064b4825b1730
Damien Fayet <damien.fayet@michelin.com>
2024-11-15 17:02:58 +0100

    update registration system

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 # accounts/urls.py
 from django.urls import path
-
+from . import views
 from .views import SignUpView
 
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,13 +2,20 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.contrib.auth import login
+from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
 
 class SignUpView(CreateView):
     form_class = UserRegisterForm
-    success_url = reverse_lazy("login")
+    success_url = reverse_lazy("home")
     template_name = "registration/signup.html"
     
+    def form_valid(self, form):
+        valid = super(SignUpView, self).form_valid(form)
+        password = form.cleaned_data.get('password1')
+        #user = authenticate( password=password)
+        user = form.save()
+        login(self.request, user)
+        return valid
